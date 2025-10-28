a7367b39b0d17d9c7ef3f4c63e6cb472921a715a
tomoki013 <tomoki_ttttt@icloud.com>
2025-10-28 22:17:10 +0900

    PWA対応

--- next.config.ts
+++ next.config.ts
@@ -7,3 +7,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // next.js config
+});
